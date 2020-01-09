f47c22a406201a56bd67653702566f50c83ef8c9
OneSolutionsGT <38265654+onesolutionsGT@users.noreply.github.com>
2020-01-09 15:36:46 -0600

    Updates

---
+++ precache-manifest.727bbc64eb50a140363525b503eec525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c776ed9717206cc8e63cd492da4ab72",
+    "url": "/test-proceres/./index.html"
+  },
+  {
+    "revision": "8f508c7161c97422c9a6",
+    "url": "/test-proceres/./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b29a466247c3f46c7182",
+    "url": "/test-proceres/./static/js/2.75dedd38.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-proceres/./static/js/2.75dedd38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f508c7161c97422c9a6",
+    "url": "/test-proceres/./static/js/main.49646531.chunk.js"
+  },
+  {
+    "revision": "4e711d536730cc779905",
+    "url": "/test-proceres/./static/js/runtime-main.b6a3d92c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-proceres/./static/media/logo.5d5d9eef.svg"
+  }
+]);
